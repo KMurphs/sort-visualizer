cd03fe8e3f111fb469f27d72be8e757a6153f1f0
Stephane <kibongesp@gmail.com>
2022-07-16 10:51:07 +0200

    Add first sorting alg 'heapsort'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,9 +10,51 @@
 	import Modal from "../components/Modal/Modal.svelte";
 	let isModalShowing = false;
 
+    const randomFromRange = (max = 100, min = 0)=>Math.floor(Math.random()*(max-min+1)+min);
+
+    const sortData = {
+        asWas: [],
+        asIs: [],
+        defaultSize: 10,
+    }
+
     const handleNavClick = (navItem) => {
         console.log(navItem)
         if(navItem === "fa-gears") isModalShowing = true;
+        if(navItem === "fa-chart-simple") { sortData.asIs = Array.from(Array(sortData.defaultSize)).map(randomFromRange); sortData.asWas = Array.from(sortData.asIs); }
+        console.log(sortData)
+        heapSort(sortData.asIs)
+        console.log(sortData)
+
+    }
+
+
+    const heapSort = (arr)=>{
+        for(let i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, arr.length, i);
+        }
+        for(let i = arr.length - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+    const heapify = (heap, heapSize, start)=>{
+        const lChild = 2 * start + 1;
+        const rChild = 2 * start + 2;
+
+        let maxFamily = start;
+        if(lChild < heapSize && heap[lChild] > heap[maxFamily]) maxFamily = lChild;
+        if(rChild < heapSize && heap[rChild] > heap[maxFamily]) maxFamily = rChild;
+
+        if(maxFamily !== start){
+            swap(heap, maxFamily, start);
+            heapify(heap, heapSize, maxFamily);
+        }
+    }
+    const swap = (arr, l, r) => {
+        const tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
     }
 
 </script>
